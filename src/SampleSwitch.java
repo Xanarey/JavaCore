d0fe87e3f3e339fdd7678fc48a9e2ed436de2923
Xanarey <doctor.layout@mail.ru>
2021-12-05 21:53:09 +0300

    5.12.21

--- src/SampleSwitch.java
+++ src/SampleSwitch.java
@@ -1,5 +1,21 @@
 public class SampleSwitch {
     public static void main(String[] args) {
-
+        for(int i = 0; i < 6; i++)
+            switch (i) {
+                case 0:
+                    System.out.println("i = " + i);
+                    break;
+                case 1:
+                    System.out.println("i = " + i);
+                    break;
+                case 2:
+                    System.out.println("i = " + i);
+                    break;
+                case 3:
+                    System.out.println("i = " + i);
+                    break;
+                default:
+                    System.out.println("i > 3");
+            }
     }
 }
