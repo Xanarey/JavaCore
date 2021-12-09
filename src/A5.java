a15682d9fb1b454252d9458fa8e29797b1ed3129
Xanarey <doctor.layout@mail.ru>
2021-12-09 20:02:43 +0300

    Windows Timur_Brek 09.12.21

---
+++ src/A5.java
@@ -0,0 +1,34 @@
+class A5 {
+    int i, j;
+
+    A5(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+class B5 extends A5 {
+    int k;
+
+
+    B5(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    void show() {
+        super.show();
+        System.out.println("k: " + k);
+    }
+}
+
+class Override {
+    public static void main(String[] args) {
+        B5 b5 = new B5(1,2,3);
+        b5.show();
+    }
+}
---
+++ src/A6.java
@@ -0,0 +1,35 @@
+class A6 {
+    int i, j;
+
+    A6(int a, int b) {
+        i = a;
+        j = b;
+    }
+
+    void show() {
+        System.out.println("i and j: " + i + " " + j);
+    }
+}
+
+class B6 extends A6 {
+    int k;
+
+    B6(int a, int b, int c) {
+        super(a, b);
+        k = c;
+    }
+
+    void show(String msg) {
+        System.out.println(msg + k);
+    }
+}
+
+class Override2 {
+    public static void main(String[] args) {
+        B6 b6 = new B6(1,2,3);
+
+        b6.show("Это k: ");
+
+        b6.show();
+    }
+}
