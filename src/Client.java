5431403887ef75f91eeb4bf67136a413f0725cee
Xanarey <doctor.layout@mail.ru>
2021-12-11 16:57:43 +0300

    Windows Timur_Brek 09.12.21

---
+++ src/Client.java
@@ -0,0 +1,10 @@
+public class Client implements Callback{
+
+    public void callback(int param) {
+        System.out.println("Метод callback() вызываемый со значением: " + param);
+    }
+
+    void nonIfaceMeth() {
+        System.out.println("В классах реализующих инт, могут быть и др. члены");
+    }
+}
--- src/p2/OtherPackage.java
+++ src/p2/OtherPackage.java
@@ -4,7 +4,7 @@ import p1.Protection;
 
 public class OtherPackage {
     OtherPackage() {
-        p1.Protection p = new p1.Protection();
+        Protection p = new Protection();
         System.out.println("Конструктор из " +
                 "другого пакета");
         System.out.println("n_pub = " + p.n_pub);
