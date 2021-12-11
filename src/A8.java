102ee380866558589f92e954096161fb73c20af2
Xanarey <doctor.layout@mail.ru>
2021-12-11 10:07:17 +0300

    Windows Timur_Brek 09.12.21

---
+++ src/A8.java
@@ -0,0 +1,24 @@
+abstract class A8 {
+
+    abstract void callme();
+
+    void callmetoo() {
+        System.out.println("Конкретный метод");
+    }
+}
+
+class B8 extends A8 {
+
+    void callme() {
+        System.out.println("Реализация метода callme() в классе В");
+    }
+}
+
+class AbstractDemo {
+    public static void main(String[] args) {
+        B8 b8 = new B8();
+
+        b8.callme();
+        b8.callmetoo();
+    }
+}
