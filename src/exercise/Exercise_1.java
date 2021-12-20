5a157bbe1820c7d9ccc3393ad83a937e4e2e959c
Xanarey <doctor.layout@mail.ru>
2021-12-20 19:21:16 +0300

    Calc v.2

--- src/exercise/Exercise_1.java
+++ src/exercise/Exercise_1.java
@@ -10,17 +10,19 @@ class Exercise_1 {
     }
 
     public static int[] getValue(int[] array, int value) {
-        int[] arr = array;
-        int[] res = new int[2];
-        int size = arr.length;
-        int i, j = 0;
 
-        for(j = size - 1; j > 0; --j)
-            for(i = j; i > 0; i--)
-                if(arr[j] + arr[i] == value) {
-                    res[0] = i;
-                    res[1] = j;
+        int[] res = {0, 0};
+        int size = array.length;
+        int i, j, sum = 0;
+
+        for(j = 1; j < size - 1; j++)
+            for(i = j; i < size - 1; i++) {
+                sum = array[j] + array[i];
+                if(sum == value) {
+                    res[0] = j;
+                    res[1] = j + 1;
                 }
+            }
         return res;
     }
 }
