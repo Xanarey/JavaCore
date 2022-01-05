67247400e8703944e9893785b820ffa7af8ef87f
Xanarey <doctor.layout@mail.ru>
2022-01-05 21:16:02 +0300

    exe

--- src/exercise/Exercise_1.java
+++ src/exercise/Exercise_1.java
@@ -1,32 +1,24 @@
 package exercise;
 
-import java.util.Arrays;
+
+import java.util.*;
 
 class Exercise_1 {
 
     public static void main(String[] args) {
-        int[] a = {3, 8, 15, 17};
+        int[] a = {15, 7, 8, 1};
         System.out.println(Arrays.toString(getValue(a, 23)));
     }
 
     public static int[] getValue(int[] array, int value) {
 
-        int[] res = {0, 0};
-        int size = array.length;
-        int i, j, sum = 0;
-
-        outer:
-        for(j = 0; j < size; j++)
-            for(i = j + 1; i < size; i++) {
-                sum = array[j] + array[i];
-                if(sum == value) {
-                    res[0] = j;
-                    res[1] = i;
-                    break outer;
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < array.length; i++) {
+            if(map.containsKey(array[i]) && i != map.get(array[i])) {
+                return new int[] {map.get(array[i]), i};
             }
-        return res;
+            map.put(value - array[i], i);
+        }
+        return new int[2];
     }
 }
-
-// [3, 8, 15, 17]       /         23
