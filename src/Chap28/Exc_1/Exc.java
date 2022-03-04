978e4ce9d458592edf16670050a1c55afe7a0fb6
Xanarey <doctor.layout@mail.ru>
2022-03-04 22:53:59 +0300

    topor

--- src/Chap28/Exc_1/Exc.java
+++ src/Chap28/Exc_1/Exc.java
@@ -1,31 +1,72 @@
 package Chap28.Exc_1;
 
 public class Exc {
-
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         Foo foo = new Foo();
 
-        foo.second(new Thread());
-        foo.first(new Thread());
-        foo.third(new Thread());
+        Thread A = new Thread();
+        A.start();
+        Thread B = new Thread();
+        B.start();
+        Thread C = new Thread();
+        C.start();
+
+        foo.first(A);
+        foo.third(C);
+        foo.second(B);
 
     }
 }
 
 class Foo {
+    public int count;
 
-    public void first(Runnable r) {
-
-        System.out.print("first");
+    public Foo() {
+        this.count = 1;
     }
 
-    public void second(Runnable r) {
-
-        System.out.print("second");
+    synchronized public void first(Runnable r)  {
+        while (count != 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        r.run();
+        count++;
+        System.out.println("first");
+        System.out.println(r);
+        notifyAll();
     }
 
-    public void third(Runnable r) {
+    synchronized public void second(Runnable r)  {
+        while (count != 2) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        r.run();
+        count++;
+        System.out.println("second");
+        System.out.println(r);
+        notifyAll();
+    }
 
-        System.out.print("third");
+    synchronized public void third(Runnable r)  {
+        while (count != 3) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        r.run();
+        System.out.println("third");
+        System.out.println(r);
+        notifyAll();
     }
 }
+
