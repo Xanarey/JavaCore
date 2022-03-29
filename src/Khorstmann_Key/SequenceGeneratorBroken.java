e774b65a2bb8499752824294dfcb9f59007c90a0
Xanarey <doctor.layout@mail.ru>
2022-03-29 20:33:01 +0300

    Atomic

---
+++ src/Khorstmann_Key/SequenceGeneratorBroken.java
@@ -0,0 +1,29 @@
+package Khorstmann_Key;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SequenceGeneratorBroken {
+
+    private static final AtomicInteger counter = new AtomicInteger();
+
+    public static int nextInt() { return counter.getAndIncrement(); }
+
+    public static void main(String[] args) throws InterruptedException {
+        List<Thread> threads = new ArrayList<>();
+
+        for (int i = 0; i < 10; ++i) {
+            Thread thread = new Thread( () -> {
+                for (int j = 0; j < 1000; ++j) nextInt();});
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread thread :  threads)
+            thread.join();
+
+        System.out.println("Counter final value: " + counter);
+    }
+
+}
