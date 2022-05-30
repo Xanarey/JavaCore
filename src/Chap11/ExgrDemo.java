38341c2d04423a26477079c5016d72a10eb5e3b3
Xanarey <doctor.layout@mail.ru>
2022-05-30 18:39:29 +0300

    MLTHRD

--- src/Chap11/ExgrDemo.java
+++ src/Chap11/ExgrDemo.java
@@ -1,6 +1,8 @@
 package Chap11;
 
+import java.util.ArrayList;
 import java.util.concurrent.Exchanger;
+import java.util.concurrent.Phaser;
 
 public class ExgrDemo {
 
@@ -44,3 +46,119 @@ public class ExgrDemo {
         }
     }
 }
+
+class Bus {
+    private static final Phaser PHASER = new Phaser(1);
+
+    public static void main(String[] args) {
+        ArrayList<Passenger> passengers = new ArrayList<>();
+
+        for (int i = 1; i < 5; i++) {           //Сгенерируем пассажиров на остановках
+            if ((int) (Math.random() * 2) > 0)
+                passengers.add(new Passenger(i, i + 1));//Этот пассажир выходит на следующей
+
+            if ((int) (Math.random() * 2) > 0)
+                passengers.add(new Passenger(i, 5));    //Этот пассажир выходит на конечной
+        }
+
+        for (int i = 0; i < 7; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Автобус выехал из парка.");
+                    PHASER.arrive();//В фазе 0 всего 1 участник - автобус
+                    break;
+                case 6:
+                    System.out.println("Автобус уехал в парк.");
+                    PHASER.arriveAndDeregister();//Снимаем главный поток, ломаем барьер
+                    break;
+                default:
+                    int currentBusStop = PHASER.getPhase();
+                    System.out.println("Остановка № " + currentBusStop);
+
+                    for (Passenger p : passengers)          //Проверяем, есть ли пассажиры на остановке
+                        if (p.departure == currentBusStop) {
+                            PHASER.register();//Регистрируем поток, который будет участвовать в фазах
+                            p.start();        // и запускаем
+                        }
+
+                    PHASER.arriveAndAwaitAdvance();//Сообщаем о своей готовности
+            }
+        }
+    }
+
+    public static class Passenger extends Thread {
+        private int departure;
+        private int destination;
+
+        public Passenger(int departure, int destination) {
+            this.departure = departure;
+            this.destination = destination;
+            System.out.println(this + " ждёт на остановке № " + this.departure);
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                System.out.println(this + " сел в автобус.");
+
+                while (PHASER.getPhase() < destination)
+                    PHASER.arriveAndAwaitAdvance();
+
+                Thread.sleep(1);
+                System.out.println(this + " покинул автобус.");
+                PHASER.arriveAndDeregister();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "Пассажир{" + departure + " -> " + destination + '}';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
