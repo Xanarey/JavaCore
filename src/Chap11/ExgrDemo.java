0f8304c4383a695b703bc1472936e240d15b19f0
Xanarey <doctor.layout@mail.ru>
2022-05-30 18:20:37 +0300

    MLTHRD

--- src/Chap11/ExgrDemo.java
+++ src/Chap11/ExgrDemo.java
@@ -1,4 +1,46 @@
 package Chap11;
 
+import java.util.concurrent.Exchanger;
+
 public class ExgrDemo {
+
+    private static final Exchanger<String> EXCHANGER = new Exchanger<>();
+
+    public static void main(String[] args) {
+        String[] p1 = new String[]{"{посылка A->D}", "{посылка A->C}"};
+        String[] p2 = new String[]{"{посылка B->C}", "{посылка B->D}"};
+
+        new Thread(new Truck(1, "A", "D", p1)).start();
+        new Thread(new Truck(2, "B", "C", p2)).start();
+    }
+
+    public static class Truck implements Runnable {
+        private int number;
+        private String dep;
+        private String dest;
+        private String[] parcels;
+
+        public Truck(int number, String dep, String dest, String[] parcels) {
+            this.number = number;
+            this.dep = dep;
+            this.dest = dest;
+            this.parcels = parcels;
+        }
+
+        @Override
+        public void run() {
+            try {
+                System.out.printf("Грузовик №%d выехал из пункта %s в пункт %s.\n", number, dep, dest);
+                System.out.printf("В грузовик №%d погрузили: %s и %s.\n", number, parcels[0], parcels[1]);
+                Thread.sleep(1000);
+                System.out.printf("Грузовик №%d приехал в пункт Е.\n", number);
+                parcels[1] = EXCHANGER.exchange(parcels[1]);
+                System.out.printf("В грузовик №%d переместили посылку для пункта %s.\n", number, dest);
+                Thread.sleep(1000 + (long) (Math.random() * 5000));
+                System.out.printf("Грузовик №%d приехал в %s и доставил: %s и %s.\n", number, dest, parcels[0], parcels[1]);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
